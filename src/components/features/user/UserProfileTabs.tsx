2ccf45848846312ed77bedc2d4b960b00ecca6bb
Lan Anh <dhlananh2309@gmail.com>
2025-09-25 12:36:43 +0700

    refactor: improve UserProfileTabs layout and add border to post content

--- src/components/features/user/UserProfileTabs.tsx
+++ src/components/features/user/UserProfileTabs.tsx
@@ -2,13 +2,13 @@
 
 
 import React from "react";
-import { FollowList } from "@/components/features/user/FollowList";
 import {
   Tabs,
   TabsContent,
   TabsList,
   TabsTrigger
 } from "@/components/ui/tabs";
+import { FollowList } from "./FollowList";
 
 
 interface UserProfileTabsProps {
@@ -18,26 +18,26 @@ interface UserProfileTabsProps {
 
 export function UserProfileTabs({ userId }: UserProfileTabsProps) {
   return (
-    <Tabs
-      defaultValue="posts"
-      className="w-full"
-    >
+    <Tabs defaultValue="posts" className="w-full">
       <TabsList className="grid w-full grid-cols-3">
         <TabsTrigger value="posts">Posts</TabsTrigger>
         <TabsTrigger value="followers">Followers</TabsTrigger>
         <TabsTrigger value="following">Following</TabsTrigger>
       </TabsList>
+
       <TabsContent value="posts">
-        <div className="p-4 text-center text-muted-foreground">
+        <div className="p-10 text-center text-muted-foreground border rounded-md">
           Posts feature coming soon!
         </div>
       </TabsContent>
+
       <TabsContent value="followers">
         <FollowList
           userId={ userId }
           type="followers"
         />
       </TabsContent>
+
       <TabsContent value="following">
         <FollowList
           userId={ userId }
