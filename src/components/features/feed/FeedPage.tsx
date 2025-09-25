082e7a54bfe67481eff1beea78a5ce865710602c
Lan Anh <dhlananh2309@gmail.com>
2025-09-25 17:24:59 +0700

    feat: add navbar to feed page

--- src/components/features/feed/FeedPage.tsx
+++ src/components/features/feed/FeedPage.tsx
@@ -5,11 +5,15 @@ import React from "react";
 import CreatePostWidget from "@/components/shared/CreatePostWidget";
 // import PostFeed from "@/components/features/post/PostFeed";
 import TopCommunitiesWidget from "@/components/features/community/widgets/TopCommunitiesWidget";
+import { Navbar } from "@/components/shared/Navbar";
 
 
 export default function FeedPage() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-6 py-6">
+      <div className="relative flex min-h-screen flex-col">
+        <Navbar />
+      </div>
       <div className="md:col-span-2">
         <CreatePostWidget />
         {/* <PostFeed /> */ }
