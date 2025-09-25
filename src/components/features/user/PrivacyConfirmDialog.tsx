fc57705e1205c8bc3874c5fe2d2255cb6754e8fa
Lan Anh <dhlananh2309@gmail.com>
2025-09-25 18:10:58 +0700

    feat: add PrivacyConfirmDialog component for account privacy changes

---
+++ src/components/features/user/PrivacyConfirmDialog.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+
+import React from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+
+interface PrivacyConfirmDialogProps {
+  isOpen: boolean;
+  onOpenChange: (open: boolean) => void;
+  onConfirm: () => void;
+  isMakingPrivate: boolean;
+}
+
+
+export function PrivacyConfirmDialog({
+  isOpen,
+  onOpenChange,
+  onConfirm,
+  isMakingPrivate,
+}: PrivacyConfirmDialogProps) {
+  const title = isMakingPrivate
+    ? "Make Account Private?" : "Make Account Public?";
+
+  const description = isMakingPrivate
+    ? "Only your followers will be able to see your posts and activity. Do you want to continue?"
+    : "Your profile and posts will be visible to everyone. Do you want to continue?";
+
+  return (
+    <AlertDialog
+      open={ isOpen }
+      onOpenChange={ onOpenChange
+
+      }>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            { title }
+          </AlertDialogTitle>
+          <AlertDialogDescription>
+            { description }
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction
+            onClick={ onConfirm }
+          >
+            Continue
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+}
