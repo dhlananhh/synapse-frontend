66b8edbfec2cb3b46d4979b5054b09ec901cdd96
Lan Anh <dhlananh2309@gmail.com>
2025-09-25 17:26:33 +0700

    style: move Navbar outside of grid container

--- src/components/features/feed/FeedPage.tsx
+++ src/components/features/feed/FeedPage.tsx
@@ -11,13 +11,11 @@ import { Navbar } from "@/components/shared/Navbar";
 export default function FeedPage() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-6 py-6">
-      <div className="relative flex min-h-screen flex-col">
-        <Navbar />
+      <Navbar />
 
-        <div className="md:col-span-2">
-          <CreatePostWidget />
-          {/* <PostFeed /> */ }
-        </div>
+      <div className="md:col-span-2">
+        <CreatePostWidget />
+        {/* <PostFeed /> */ }
       </div>
       <aside className="hidden md:block">
         <div className="sticky top-20 space-y-4">
