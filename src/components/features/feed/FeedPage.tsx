c6e6a9a20fde27e8577d8857e79c12914aaa7bc7
Lan Anh <dhlananh2309@gmail.com>
2025-09-25 17:25:46 +0700

    refactor: move CreatePostWidget to within Navbar component

--- src/components/features/feed/FeedPage.tsx
+++ src/components/features/feed/FeedPage.tsx
@@ -13,10 +13,11 @@ export default function FeedPage() {
     <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-6 py-6">
       <div className="relative flex min-h-screen flex-col">
         <Navbar />
-      </div>
-      <div className="md:col-span-2">
-        <CreatePostWidget />
-        {/* <PostFeed /> */ }
+
+        <div className="md:col-span-2">
+          <CreatePostWidget />
+          {/* <PostFeed /> */ }
+        </div>
       </div>
       <aside className="hidden md:block">
         <div className="sticky top-20 space-y-4">
