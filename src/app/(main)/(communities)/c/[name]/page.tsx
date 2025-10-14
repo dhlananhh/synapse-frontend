63d6c8a016f0619f6034351ac90bb53c8ba1672c
dhlananhh <dhlananh2309@gmail.com>
2025-10-14 15:50:00 +0700

    chore: remove post feed from community page

--- src/app/(main)/(communities)/c/[name]/page.tsx
+++ src/app/(main)/(communities)/c/[name]/page.tsx
@@ -2,7 +2,6 @@
 
 import { useSearchParams } from "next/navigation";
 import CommunityHeader from "@/components/features/community/CommunityHeader";
-import CommunityPostFeed from "@/components/features/community/CommunityPostFeed";
 import { useCommunity } from "@/context/CommunityContext";
 import { useMembership } from "@/context/MembershipContext";
 import { format } from "date-fns";
@@ -19,13 +18,11 @@ export default function CommunityPage() {
   return (
     <div>
       <CommunityHeader
-        community={community}
-        membership={membership}
-        onMembershipChange={setMembership}
+        community={ community }
+        membership={ membership }
+        onMembershipChange={ setMembership }
       />
-      {/* <CreatePostWidget /> */}
       <div className="mt-6">
-        {/* <CommunityPostFeed communityId={community.id} flairId={activeFlairId} /> */}
       </div>
       <span></span>
     </div>
