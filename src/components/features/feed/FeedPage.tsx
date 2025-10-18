82c699dc5c0171417815043c960b6e75d89c7364
dhlananhh <dhlananh2309@gmail.com>
2025-10-18 21:44:05 +0700

    refactor: move TopCommunitiesWidget to the left and adjust FeedPage layout

--- src/components/features/feed/FeedPage.tsx
+++ src/components/features/feed/FeedPage.tsx
@@ -1,23 +1,24 @@
 "use client";
 
+
 import React from "react";
-import CreatePostWidget from "@/components/shared/CreatePostWidget";
 import { Navbar } from "@/components/shared/Navbar";
 import TopCommunitiesWidget from "@/components/features/community/widgets/TopCommunitiesWidget";
+
 
 export default function FeedPage() {
   return (
     <div className="grid grid-cols-1 gap-y-4 py-6 md:grid-cols-3 md:gap-x-6">
       <Navbar />
 
-      <div className="md:col-span-2">
-        <CreatePostWidget />
+      <div className="grid grid-cols-1 md:grid-cols-[420px_1fr] lg:grid-cols-[420px_1fr] gap-8">
+        <aside className="hidden lg:block">
+          <div className="sticky top-4 space-y-4">
+            <TopCommunitiesWidget />
+          </div>
+        </aside>
       </div>
-      <aside className="hidden md:block">
-        <div className="sticky top-20 space-y-4">
-          <TopCommunitiesWidget />
-        </div>
-      </aside>
+
     </div>
   );
 }
